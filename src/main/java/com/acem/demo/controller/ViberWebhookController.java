432a28de2b544d58260b05e09f5f57e1a901d4bb
bishnukdk <bishnukhadka442@gmail.com>
2023-02-01 17:01:27 +0545

    modified ViberResponse to fit viber's response to set_webhook

--- src/main/java/com/acem/demo/controller/ViberWebhookController.java
+++ src/main/java/com/acem/demo/controller/ViberWebhookController.java
@@ -24,7 +24,7 @@ public class ViberWebhookController {
     public ResponseEntity<String> setWebhook(){
         String requestUrl = VIBER_API_URL + "set_webhook";
         String requestBody = "{\"url\":\"" + viberWebhookUrl + "\",\"event_types\":[\"delivered\",\"failed\",\"subscribed\",\"unsubscribed\",\"conversation_started\"]}";
-        System.out.println(requestBody);
+        System.out.println("Request Body: " + requestBody);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.add("X-Viber-Auth-Token", VIBER_AUTH_TOKEN);
@@ -33,10 +33,14 @@ public class ViberWebhookController {
         RestTemplate restTemplate = new RestTemplate();
 
         ResponseEntity<String> response = restTemplate.postForEntity(requestUrl, request, String.class);
+        System.out.println("Viber response: " + response);
+        System.out.println("Viber Response body" + response.getBody());
+
+        System.out.println("Viber response status code: " + response.getStatusCode());
 
         //check if viber webhook check performed or not.
         //viber responds with status code 200 if checked correctly.
-        if(response.getStatusCode().equals(200)){
+        if(response.getStatusCode().toString().equals("200 OK")){
             System.out.println("Viber check completed successfully");
         }else
             System.out.println("Problem while checking webhook availability by viber.");
@@ -47,7 +51,7 @@ public class ViberWebhookController {
 
         //check if the response from the viber server has status:0 or not?
         if(viberResponse.getStatus()==0)
-            return ResponseEntity.status(HttpStatus.OK).body(json+"\nWebhook set successfully.");
+            return ResponseEntity.status(HttpStatus.OK).body("Webhook set successfully.");
         else
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(json);
@@ -60,37 +64,37 @@ public class ViberWebhookController {
 
     @PostMapping
     public ResponseEntity<String> handleWebhookEvent(@RequestBody String requestBody) {
-        System.out.println(requestBody);
-//
-//        // Parse the request body as a JSON object
-//        EventJsonRequest eventJsonRequest;
-//        eventJsonRequest = JacksonUtil.fromJson(requestBody, EventJsonRequest.class);
-//
-//        // Extract the event type from the request body
-//        String eventType = eventJsonRequest.getEventTypes().get(0);
-//
-//        // Handle the event based on its type
-//        switch (eventType) {
-//            case "message":
-//                // Handle a message event
-//                System.out.println("message event");
-//                break;
-//            case "subscribed":
-//                // Handle a subscribed event
-//                System.out.println("subscribed event");
-//                break;
-//            case "unsubscribed":
-//                // Handle an unsubscribed event
-//                System.out.println("unsubscribed event");
-//                break;
-//            case "conversation_started":
-//                //Handle conversation_started event
-//                System.out.println("conversation_started event");
-//            default:
-//                // Handle any other unknown event types
-//                System.out.println("default event");
-//                break;
-//        }
+        System.out.println("HandleWebhookEvent requestBody: " + requestBody);
+
+        // Parse the request body as a JSON object
+        EventJsonRequest eventJsonRequest;
+        eventJsonRequest = JacksonUtil.fromJson(requestBody, EventJsonRequest.class);
+
+        // Extract the event type from the request body
+        String eventType = eventJsonRequest.getEventTypes().get(0);
+
+        // Handle the event based on its type
+        switch (eventType) {
+            case "message":
+                // Handle a message event
+                System.out.println("message event");
+                break;
+            case "subscribed":
+                // Handle a subscribed event
+                System.out.println("subscribed event");
+                break;
+            case "unsubscribed":
+                // Handle an unsubscribed event
+                System.out.println("unsubscribed event");
+                break;
+            case "conversation_started":
+                //Handle conversation_started event
+                System.out.println("conversation_started event");
+            default:
+                // Handle any other unknown event types
+                System.out.println("default event");
+                break;
+        }
 
         // Return a success response
         return new ResponseEntity<>("OK", HttpStatus.OK);
--- src/main/java/com/acem/demo/response/ViberResponse.java
+++ src/main/java/com/acem/demo/response/ViberResponse.java
@@ -5,11 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class ViberResponse {
     private Integer status;
+    private String status_message;
     private String chat_hostname;
+    private List<String> event_types;
 }
